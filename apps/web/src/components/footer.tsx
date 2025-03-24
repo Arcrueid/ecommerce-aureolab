a6abc5b56d6fac32bf8bf38e3f5141e8c888af02
Arcrueid <fierro.jp@gmail.com>
2025-03-24 11:36:03 -0300

    feat(wip): add footer

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,7 +1,54 @@
+import { Link } from "@tanstack/react-router";
+import { FacebookIcon, LinkedinIcon } from "lucide-react";
+
 export const Footer = () => {
   return (
-    <div className="container mx-auto mt-auto flex items-center justify-center gap-2 p-2">
-      Footer
-    </div>
+    <footer className="mt-auto flex min-h-40 flex-col border-t border-gray-200 bg-[#161b1e]">
+      <div className="container mx-auto flex items-center justify-center gap-2 p-12">
+        <div className="flex w-full flex-row items-center justify-between gap-8">
+          <Link to="/" className="flex" aria-label="Ir a la página principal">
+            <img
+              src="/logo-aureolab-white.svg"
+              alt="Aureolab"
+              className="w-36"
+            />
+          </Link>
+
+          <div className="flex flex-1 items-center justify-end gap-2">
+            <nav
+              className="flex flex-row items-center justify-center gap-8"
+              aria-label="Redes sociales"
+            >
+              <div className="text-sm text-gray-400">Síguenos</div>
+              <ul className="flex items-center gap-4">
+                <li>
+                  <a
+                    href="https://www.facebook.com/aureolabcl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Facebook de Aureolab"
+                  >
+                    <FacebookIcon className="size-6 text-gray-400 hover:text-[#fed137]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/aureolab"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="LinkedIn de Aureolab"
+                  >
+                    <LinkedinIcon className="size-6 text-gray-400 hover:text-[#fed137]" />
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto mt-auto mb-3 flex items-center justify-center gap-2 p-2 text-sm font-light text-gray-400">
+        <small>© 2025 Aureolab. All rights reserved.</small>
+      </div>
+    </footer>
   );
 };
